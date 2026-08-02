b2b24bc5f00786ef1fba9e437a384f9765454c7d
lee <dldbsgh00005@gmail.com>
2026-08-02 21:11:56 +0900

    Skip absent legacy intensity table in migration

--- db/migration/V22__us_legacy_instrument_links.sql
+++ db/migration/V22__us_legacy_instrument_links.sql
@@ -4,9 +4,6 @@ ALTER TABLE us_turnover_ratio_blacklist
 ALTER TABLE us_news_ticker_exchange_cache
   ADD COLUMN IF NOT EXISTS instrument_id BIGINT;
 
-ALTER TABLE us_intensity_stocks
-  ADD COLUMN IF NOT EXISTS instrument_id BIGINT;
-
 UPDATE us_news_ticker_exchange_cache c
 SET instrument_id = i.id
 FROM us_instruments i
@@ -20,12 +17,6 @@ FROM us_instruments i
 WHERE i.code = b.ticker
   AND b.instrument_id IS NULL;
 
-UPDATE us_intensity_stocks s
-SET instrument_id = i.id
-FROM us_instruments i
-WHERE i.code = s.code
-  AND s.instrument_id IS NULL;
-
 DO $$
 BEGIN
   IF NOT EXISTS (SELECT 1 FROM pg_constraint WHERE conname = 'us_turnover_ratio_blacklist_instrument_fk') THEN
@@ -34,10 +25,6 @@ BEGIN
   END IF;
   IF NOT EXISTS (SELECT 1 FROM pg_constraint WHERE conname = 'us_news_ticker_exchange_cache_instrument_fk') THEN
     ALTER TABLE us_news_ticker_exchange_cache ADD CONSTRAINT us_news_ticker_exchange_cache_instrument_fk
-      FOREIGN KEY (instrument_id) REFERENCES us_instruments(id);
-  END IF;
-  IF NOT EXISTS (SELECT 1 FROM pg_constraint WHERE conname = 'us_intensity_stocks_instrument_fk') THEN
-    ALTER TABLE us_intensity_stocks ADD CONSTRAINT us_intensity_stocks_instrument_fk
       FOREIGN KEY (instrument_id) REFERENCES us_instruments(id);
   END IF;
 END $$;
