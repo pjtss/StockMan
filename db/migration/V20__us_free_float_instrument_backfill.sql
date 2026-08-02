20f3da0abd4663677a41f974052c46672dbec26f
lee <dldbsgh00005@gmail.com>
2026-08-02 21:04:41 +0900

    Fix free float instrument backfill join

--- db/migration/V20__us_free_float_instrument_backfill.sql
+++ db/migration/V20__us_free_float_instrument_backfill.sql
@@ -1,4 +1,7 @@
 UPDATE us_free_float_snapshots f SET instrument_id = i.id
 FROM us_news_ticker_exchange_cache c
-JOIN us_instruments i ON i.market = c.market AND i.code = f.ticker
-WHERE c.ticker = f.ticker AND f.instrument_id IS NULL;
+, us_instruments i
+WHERE c.ticker = f.ticker
+  AND i.market = c.market
+  AND i.code = f.ticker
+  AND f.instrument_id IS NULL;
